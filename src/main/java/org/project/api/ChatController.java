216b9a2aee344b5964995a823be61f9b9c0fa7d3
GhostASA <artemsvist1809@gmail.com>
2025-03-11 17:26:14 +0300

    online chat websocket

---
+++ src/main/java/org/project/api/ChatController.java
@@ -0,0 +1,17 @@
+package org.project.api;
+
+
+import org.project.dto.ChatMessage;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ChatController {
+
+    @MessageMapping("/sendMessage")
+    @SendTo("/topic/messages")
+    public ChatMessage sendMessage(ChatMessage message) {
+        return message;
+    }
+}
---
+++ src/main/java/org/project/dto/ChatMessage.java
@@ -0,0 +1,12 @@
+package org.project.dto;
+
+import lombok.Data;
+
+@Data
+public class ChatMessage {
+
+    private String content;
+
+    private String sender;
+
+}
